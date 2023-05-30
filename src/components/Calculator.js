966d806e164cd51726cf80f00f22be4d0b6903b1
Luiscarlosvd <lc26cr7@gmail.com>
2023-05-30 17:55:25 -0400

    Add onClick event in buttonsSection.js to get the value of each button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,10 +3,13 @@ import './calculator.css';
 import ButtonsSection from './ButtonsSection';
 
 function Calculator() {
+  const handleValue = (e) => {
+    console.log(e.target.textContent);
+  };
   return (
     <div className="calculator-container">
       <div className="div-result">0</div>
-      <ButtonsSection />
+      <ButtonsSection onClick={handleValue} />
     </div>
   );
 }
