8fe9786ed4d70f104f73b8281f01e076d54071e7
Luiscarlosvd <lc26cr7@gmail.com>
2023-05-30 12:56:29 -0400

    Refactor Button component to ButtonsSection

---
+++ src/components/ButtonsSection.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './button.css';
+
+const ButtonsSection = () => (
+  <div className="buttons-calculator">
+    <button type="button" className="button">AC</button>
+    <button type="button" className="button">+/-</button>
+    <button type="button" className="button">%</button>
+    <button type="button" className="button">/</button>
+    <button type="button" className="button">7</button>
+    <button type="button" className="button">8</button>
+    <button type="button" className="button">9</button>
+    <button type="button" className="button">X</button>
+    <button type="button" className="button">4</button>
+    <button type="button" className="button">5</button>
+    <button type="button" className="button">6</button>
+    <button type="button" className="button">-</button>
+    <button type="button" className="button">1</button>
+    <button type="button" className="button">2</button>
+    <button type="button" className="button">3</button>
+    <button type="button" className="button">+</button>
+    <button type="button" className="button" id="button-0">0</button>
+    <button type="button" className="button">.</button>
+    <button type="button" className="button">=</button>
+  </div>
+);
+
+export default ButtonsSection;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './calculator.css';
-import Button from './Button';
+import ButtonsSection from './ButtonsSection';
 
 function Calculator() {
   return (
     <div className="calculator-container">
       <div className="div-result">0</div>
-      <div className="buttons-calculator">
-        <Button name="AC" />
-      </div>
+      <ButtonsSection />
     </div>
   );
 }
