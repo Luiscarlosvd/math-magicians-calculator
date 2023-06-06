5f110c2492c469e6f90a47fd7bdc69cade7c03e9
Luiscarlosvd <lc26cr7@gmail.com>
2023-06-06 15:43:36 -0400

    Create tests for Calculator component

--- src/tests/components.test.js
+++ src/tests/components.test.js
@@ -1,32 +1,62 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect';
 import ButtonsSection from '../components/ButtonsSection';
+import Calculator from '../components/Calculator';
+
+// ButtonSection component tests
 
 describe('ButtonsSection', () => {
-  describe('when rendered', () => {
-    it('should match the snapshot', () => {
-      // Arrange
-      const onClick = jest.fn();
+  test('should match the snapshot', () => {
+    // Arrange
+    const onClick = jest.fn();
 
-      // Act
-      const { container } = render(<ButtonsSection onClick={onClick} />);
+    // Act
+    const { container } = render(<ButtonsSection onClick={onClick} />);
 
-      // Assert
-      expect(container.firstChild).toMatchSnapshot();
-    });
+    // Assert
+    expect(container.firstChild).toMatchSnapshot();
   });
 
-  describe('when a button is clicked', () => {
-    it('should call the onClick prop', () => {
-      // Arrange
-      const onClick = jest.fn();
-      const { getByText } = render(<ButtonsSection onClick={onClick} />);
+  test('should call the onClick prop', () => {
+    // Arrange
+    const onClick = jest.fn();
+    const { getByText } = render(<ButtonsSection onClick={onClick} />);
 
-      // Act
-      fireEvent.click(getByText('1'));
+    // Act
+    fireEvent.click(getByText('1'));
 
-      // Assert
-      expect(onClick).toHaveBeenCalled();
-    });
+    // Assert
+    expect(onClick).toHaveBeenCalled();
+  });
+});
+
+// Calculator component test
+
+describe('Calculator', () => {
+  test('should match the snapshot', () => {
+    // Arrange
+
+    // Act
+    const { container } = render(<Calculator />);
+
+    // Assert
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test('should update the result with the correct value', () => {
+    // Arrange
+    const { getByText } = render(<Calculator />);
+    const expectedValue = '3';
+
+    // Act
+    fireEvent.click(getByText('1'));
+    fireEvent.click(getByText('+'));
+    fireEvent.click(getByText('2'));
+    fireEvent.click(getByText('='));
+    const resultElement = document.querySelector('.div-result');
+    
+    // Assert
+    expect(resultElement).toHaveTextContent(expectedValue);
   });
 });
