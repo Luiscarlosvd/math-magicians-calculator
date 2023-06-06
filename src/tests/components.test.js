89e2b5ca9d67c33cf1e72701402ace70630f6f4c
Luiscarlosvd <lc26cr7@gmail.com>
2023-06-06 17:25:47 -0400

    Add tests for GetQuote component

--- src/tests/components.test.js
+++ src/tests/components.test.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect';
+import axios from 'axios';
 import ButtonsSection from '../components/ButtonsSection';
 import Calculator from '../components/Calculator';
+import GetQuote from '../components/GetQuote';
 
 // ButtonSection component tests
 
@@ -58,5 +60,55 @@ describe('Calculator', () => {
     
     // Assert
     expect(resultElement).toHaveTextContent(expectedValue);
+  });
+});
+
+// GetQuote component tests
+
+describe('GetQuote', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  })
+  test('should display loading text on the screen when waiting for api response', () => {
+    // Arrange
+    jest.spyOn(axios, 'get').mockImplementation(() => new Promise(() => {}))
+    // Act
+    render(<GetQuote />);
+
+    // Assert
+    const loadingText = screen.getByText('Loading ...');
+    expect(loadingText).toBeInTheDocument();
+  });
+
+  test('should display the quote on the screen when it gets the data from the Api', async () => {
+    // Arrange
+    const responseQuote = {
+      data: [
+        {
+          quote: 'Hello',
+          author: 'Luis',
+        },
+      ],
+    };
+    jest.spyOn(axios, 'get').mockResolvedValue(responseQuote)
+
+    // Act
+    render(<GetQuote />);
+
+    // Assert
+    const quoteText = await screen.findByText(`“Hello” - Luis`);
+    expect(quoteText).toBeInTheDocument();
+  });
+
+  test('should display error message on the screen when api get method fails', async () => {
+    // Arrange
+    jest.spyOn(axios, 'get').mockRejectedValue(new Error('API request failed'));
+
+    // Act
+    render(<GetQuote />);
+
+    // Assert
+    const errorMessage = await screen.findByText('Sorry... We are experiencing some errors..');
+    expect(errorMessage).toBeInTheDocument();
   });
 });
